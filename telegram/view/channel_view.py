6bc0c8296d614cc942eb5ee574c32b289d767f59
Roman Goralchuk <roman.goralchuk@idftechnology.by>
2022-10-17 13:47:27 +0300

    feat(view): update

---
+++ telegram/view/channel_view.py
@@ -0,0 +1,52 @@
+import json
+import logging
+
+from django.http import JsonResponse
+from django.utils.decorators import method_decorator
+from django.views import View
+from django.views.decorators.csrf import csrf_exempt
+
+from telegram.service.channel_service import ChannelService
+_logger = logging.getLogger('custom')
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class ChannelHandler(View):
+	@staticmethod
+	async def get(request):
+		request_body = json.loads(request.body)
+		channel_ids: [] = request_body.get('channel_ids')
+		result = await ChannelService().findArray(channel_ids)
+		data = {
+			"channels": result
+		}
+		return JsonResponse(data)
+
+	@staticmethod
+	async def post(request):
+		request_body = json.loads(request.body)
+		channel_ids: [] = request_body.get('channel_ids')
+		result = await ChannelService().addArrayId(channel_ids)
+		data = {
+			"result": result
+		}
+		return JsonResponse(data)
+
+	@staticmethod
+	async def put(request):
+		array_channels = json.loads(request.body)
+		result = await ChannelService().addArrayDict(array_channels)
+		data = {
+			"result": result
+		}
+		return JsonResponse(data)
+
+	@staticmethod
+	async def delete(request):
+		request_body = json.loads(request.body)
+		channel_ids: [] = request_body.get('channel_ids')
+		result = await ChannelService().deleteByArrayId(channel_ids)
+		data = {
+			"result": result
+		}
+		return JsonResponse(data)
--- telegram/view/update_views.py
+++ telegram/view/update_views.py
@@ -5,18 +5,18 @@ from django.utils.decorators import method_decorator
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 
-from repository_mongo.mongo import MongoRepository
+from telegram.repository_mongo.mongo import MongoRepositoryMessages
 
 
 @method_decorator(csrf_exempt, name='dispatch')
-class Mongo(View):
+class UpdateHandler(View):
 	@staticmethod
 	async def get(request):
 		request_body = json.loads(request.body)
 		limit = request_body.get('limit')
 		message_id = request_body.get('message_id')
 		channel_id = request_body.get('channel_id')
-		result = await MongoRepository().findAll('messages', limit, message_id, channel_id)
+		result = await MongoRepositoryMessages.findAll(limit, message_id, channel_id)
 		print(result)
 		return JsonResponse(result, json_dumps_params={'ensure_ascii': False}, safe=False)
 
