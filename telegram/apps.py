a24b5a71d94818bcfb61dc4505d77eb25d23828b
Roman Goralchuk <roman.goralchuk@idftechnology.by>
2022-10-20 18:38:41 +0300

    feat(project): urls and apps

--- telegram/apps.py
+++ telegram/apps.py
@@ -1,6 +1,14 @@
+from os.path import join
+
 from django.apps import AppConfig
+
+from telegram_parser.settings import BASE_DIR
 
 
 class TelegramConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'telegram'
+
+
+TEST_DATA_FULL = join(BASE_DIR, 'telegram/test_data/full')
+TELEGRAM_SESSION = join(BASE_DIR, 'telegram/telegram_session')
--- telegram/urls.py
+++ telegram/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 
-from .views import TelegramRegistration, Mongo
+from telegram.view.channel_view import ChannelHandler
+from telegram.view.login_views import LoginHandler
+from telegram.view.task_views import TaskHandler, TaskStartHandler
+from telegram.view.update_views import UpdateHandler
 
 urlpatterns = [
-	path('registration/', TelegramRegistration.as_view()),
-	path('update/', Mongo.as_view()),
+	path('login/', LoginHandler.as_view()),
+	path('update/', UpdateHandler.as_view()),
+	path('tasks/', TaskHandler.as_view()),
+	path('tasks/start', TaskStartHandler.as_view()),
+	path('channels/', ChannelHandler.as_view()),
 ]
