4cc4c54ccf94d2a25d4c5433513821415825685f
Roman Goralchuk <roman.goralchuk@idftechnology.by>
2022-10-20 18:39:29 +0300

    feat(test_data):update starter

--- telegram/test_data/starter.py
+++ telegram/test_data/starter.py
@@ -1,11 +1,25 @@
 import asyncio
 
-from telegram.repository_mongo.mongo import MongoRepositoryMessages
+from telegram.repository_mongo.mongo_message import MongoRepositoryMessages
 from telegram.test_data.test_data_service import TestData
 
 
 async def sprint():
 
-    await MongoRepositoryMessages().saveAll(await TestData.getFullData('remoteIT_full.json'))
+    list_channel = [
+        # 'seoHR_full.json',
+        # 'Outsource_full.json',
+        'seoHR_full.json',
+        # 'work_in_gamedev_full.json',
+        # 'digitalHR_full.json',
+        # 'getITWorld_full.json',
+        # 'habr_full.json',
+        # 'jobinIT_full.json',
+        # 'telegramIT_full.json',
+        # 'workinIT_full.json'
+    ]
+    for channel in list_channel:
+        result = await MongoRepositoryMessages().saveAllDicts(await TestData.getFullData(channel))
+        print(f'{channel} - {result}')
 
 asyncio.run(sprint())
