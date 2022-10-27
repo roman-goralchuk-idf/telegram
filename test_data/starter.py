c6e7377e16290b759ccc3d84a92fbef4d2e1406e
Roman Goralchuk <roman.goralchuk@idftechnology.by>
2022-10-27 11:58:17 +0300

    refactor(rabbit): replace folder

--- test_data/starter.py
+++ test_data/starter.py
@@ -1,11 +1,26 @@
 import asyncio
 
-from repository_mongo.mongo import MongoRepository
+from telegram.repository_mongo.mongo_message import MongoRepositoryMessages
 from test_data.test_data_service import TestData
 
 
 async def sprint():
 
-    await MongoRepository().saveAll(await TestData.getFullData('work_in_gamedev_full.json'), 'messages')
+    list_channel = [
+        'seoHR_full.json',
+        'Outsource_full.json',
+        'seoHR_full.json',
+        'work_in_gamedev_full.json',
+        'digitalHR_full.json',
+        'remoteIT_full.json',
+        'getITWorld_full.json',
+        'habr_full.json',
+        'jobinIT_full.json',
+        'telegramIT_full.json',
+        'workinIT_full.json'
+    ]
+    for channel in list_channel:
+        result = await MongoRepositoryMessages().saveAllDicts(await TestData.getFullData(channel))
+        print(f'{channel} - {result}')
 
 asyncio.run(sprint())
